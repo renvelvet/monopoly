5b030d2fc37f21a852f43a34be68c63595ebcafc
Resha Puspita <reshapuspita734@gmail.com>
2019-03-31 17:01:53 +0700

    Add files via upload

--- Tile.java
+++ Tile.java
@@ -6,13 +6,13 @@ public abstract class Tile {
     protected String name;
     protected boolean isOwnable; // If isOwnable true maka dia Property, selain itu Space
     protected int groupNumber;
-    protected int ownedBy; // dimiliki oleh pemain dengan ID berapa
+    protected Player ownedBy; // dimiliki oleh pemain dengan ID berapa
 
     public Tile(String name, boolean isOwnable, int groupNumber, int price) {
         this.name = name;
         this.isOwnable = isOwnable;
         this.groupNumber = groupNumber;
-        this.ownedBy = 0;
+        this.ownedBy = null;
         this.price = price;
     }
 
@@ -21,6 +21,6 @@ public abstract class Tile {
 
     public abstract void tileAction(Player player, int i);
 
-    int getTotalTileInGroup();
+    public abstract int getTotalTileInGroup();
 
 }
