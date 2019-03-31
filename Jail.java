5a90843ab2c6fe9850cc9e4dd84087ec91bd7ffa
celesalucard <38214203+celesalucard@users.noreply.github.com>
2019-03-31 20:14:23 +0700

    added Jail.java

---
+++ Jail.java
@@ -0,0 +1,8 @@
+//tile jail merupakan tile kosong yang tidak memunculkan aksi apa-apa
+//jika player melewati atau tiba di tile jail
+//
+public class Jail extends Tile{
+    public Jail(){
+        super();
+    }    
+}
